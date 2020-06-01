451930bfbe08a3cc356507d0e5aa6e99368e9481
Renee Thomsen <rcthomsen19@gmail.com>
2020-05-31 22:56:45 -0400

    Changed arrays to "java style"

--- practice-1/src/ArrayDemo.java
+++ practice-1/src/ArrayDemo.java
@@ -1,6 +1,6 @@
 class ArrayDemo {
-    public static void main(String args[]) {
-        int sample[] = new int[10];
+    public static void main(String[] args) {
+        int[] sample = new int[10];
         int i;
         for(i = 0; i < 10; i++)
             sample[i] = i;
