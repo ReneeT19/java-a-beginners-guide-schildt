cab0a4afb9041a6b5f98c4cd4d730c8a8d757381
Renee Thomsen <rcthomsen19@gmail.com>
2020-05-31 22:56:16 -0400

    added escape character

--- practice-1/src/ArrayDemo.java
+++ practice-1/src/ArrayDemo.java
@@ -5,6 +5,6 @@ class ArrayDemo {
         for(i = 0; i < 10; i++)
             sample[i] = i;
         for(i = 0; i < 10; i++) //needs to type this for statement twice so it won't go beyond the scope of 10
-        System.out.println("This is sample[" + i + "] " + sample[i]);
+        System.out.println("This is \"sample[" + i + "]\" " + sample[i]); // \ is an escape character
     }
 }
