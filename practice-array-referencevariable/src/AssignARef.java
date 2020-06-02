260019bf637dcd5e28c6f4308c69b82517000541
Renee Thomsen <rcthomsen19@gmail.com>
2020-06-02 14:25:37 -0400

    assign an array reference

--- practice-array-referencevariable/src/AssignARef.java
+++ practice-array-referencevariable/src/AssignARef.java
@@ -3,33 +3,39 @@ public class AssignARef {
         int i;
         int[] nums1 = new int[10];
         int[] nums2 = new int[20];
-        for(i = 0; i < 10; i++)
+        for(i = 0; i < 10; i++) { //no need to put the curly brackets if only one statement is needed
             nums1[i] = i;
-        for(i = 0; i < 20; i++)
+        }
+        for(i = 0; i < 20; i++) {
             nums2[i] = i;
-
+        }
         System.out.print("Here is nums1: ");
-        for(i = 0; i < 10; i++)
+        for(i = 0; i < 10; i++) {
             System.out.print(nums1[i] + " ");
+        }
         System.out.println();
 
         System.out.print("Here is nums2: ");
-        for(i = 0; i < 20; i++)
+        for(i = 0; i < 20; i++) {
             System.out.print(nums2[i] + " ");
+        }
         System.out.println();
 
         nums2 = nums1; //assign an array reference and now nums2 refers to nums1
         System.out.print("Here is nums2 after change: ");
-        for(i = 0; i < 10; i++)
+        for(i = 0; i < 10; i++) {
             System.out.print(nums2[i] + " ");
+        }
         System.out.println();
 
         nums2[3] = 99; //operate on nums1 array through nums2
 
         System.out.print("Here is nums1 after change through nums2: ");
-        for(i = 0; i < 10; i++)
+        for(i = 0; i < 10; i++) {
             System.out.print(nums1[i] + " ");
+        }
         System.out.println();
+
     } //both nums1 and nums2 arrays refer to the same object
 }
 
