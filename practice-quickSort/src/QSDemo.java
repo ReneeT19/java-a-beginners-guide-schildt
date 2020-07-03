acd311de8edf491c25f7db24a87fb2a82ba8870e
Renee Thomsen <rcthomsen19@gmail.com>
2020-07-02 22:58:59 -0400

    Quicksort practice - char

---
+++ practice-quickSort/src/QSDemo.java
@@ -0,0 +1,17 @@
+public class QSDemo {
+    public static void main(String[] args) {
+        char a[] = {'d', 'x', 'a', 'r', 'p', 'j', 'i'};
+        int i;
+        System.out.println("original array: ");
+        for (i = 0; i < a.length; i++) {
+            System.out.print(a);
+            System.out.println();
+
+            Quicksort.qsort(a);
+            System.out.println("sorted array: ");
+            for (i = 0; i < a.length; i++) {
+                System.out.print(a[i]);
+            }
+        }
+    }
+}
